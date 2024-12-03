3835cf9458acc52b524344ffdad3de08d8eb3991
Joshua <schmitt@unately.com>
2024-12-03 08:23:48 +0100

    fix: fix

--- Formula/makepass.rb
+++ Formula/makepass.rb
@@ -6,8 +6,8 @@
 class Makepass < Formula
   desc "Simple command-line password generator"
   homepage "https://github.com/jqshuv/makepass"
-  url "https://registry.npmjs.org/makepass/-/makepass-#{version}.tgz"
   version "1.1.1"
+  url "https://registry.npmjs.org/makepass/-/makepass-#{version}.tgz"
   sha256 "ee1031e9af9886a99b496ad1739572fc592bfc364abafc13c20f97a58faafd0e"
 
   depends_on "node"
